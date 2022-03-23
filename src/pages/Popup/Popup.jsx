f78736bb194e5bc0c5a84e613e6f2e627348309a
berto-azevedo <berto.azevedo@optimizer.pt>
2022-03-23 17:48:44 +0000

    Consulta dos resultados com a api dos jogossantacasa

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,122 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+import axios from 'axios';
+import { DOMParser } from 'xmldom';
+import LogoEuromilhoes from '../../assets/img/euromilhoes.png';
+import LogoTotoloto from '../../assets/img/totoloto.png';
+import LogoMilhao from '../../assets/img/milhao.png';
 
-const Popup = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
-  );
-};
+function Result(name, key, date) {
+  this.name = name;
+  this.key = key;
+  this.date = date;
+}
+
+class Popup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      pubdate: '',
+    };
+  }
+  componentDidMount() {
+    this.GetXMLData('https://www.jogossantacasa.pt/web/SCRss/rssFeedCartRes');
+  }
+
+  ParseData(data) {
+    const parser = new DOMParser().parseFromString(data);
+
+    let pubdate = parser.getElementsByTagName('pubDate');
+    let items = parser.getElementsByTagName('item');
+
+    let results = [];
+
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i].childNodes;
+      if (
+        item[1].textContent === 'Euromilhões' ||
+        item[1].textContent === 'M1lhão' ||
+        item[1].textContent === 'Totoloto'
+      ) {
+        let result = new Result(
+          item[1].textContent,
+          item[5].textContent.split(': ')[1],
+          item[5].textContent
+            .split(': ')[0]
+            .replace('<b>', '')
+            .replace('</b>', '')
+        );
+        results.push(result);
+      }
+    }
+    this.setState({ results, pubdate: pubdate[0].textContent });
+  }
+
+  GetXMLData(url) {
+    axios
+      .get(url)
+      .then((response) => {
+        this.ParseData(response.data);
+      })
+      .catch((e) => {
+        console.log('Error: ', e.response.data);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={LogoEuromilhoes} alt="logo" style={{ marginBottom: 10 }} />
+          {this.state.results.map((r, i) => {
+            if (r.name === 'Euromilhões')
+              return (
+                <div key={i}>
+                  <div className="new-line" style={{ marginBottom: 8 }}>
+                    {r.key}
+                  </div>
+                  <div className="new-line">{r.date}</div>
+                </div>
+              );
+            else return '';
+          })}
+          <br></br>
+          <img src={LogoMilhao} alt="logo" style={{ marginBottom: 10 }} />
+          {this.state.results.map((r, i) => {
+            if (r.name === 'M1lhão')
+              return (
+                <div key={i}>
+                  <div className="new-line" style={{ marginBottom: 8 }}>
+                    {r.key}
+                  </div>
+                  <div className="new-line">{r.date}</div>
+                </div>
+              );
+            else return '';
+          })}
+          <br></br>
+          <img src={LogoTotoloto} alt="logo" style={{ marginBottom: 10 }} />
+          {this.state.results.map((r, i) => {
+            if (r.name === 'Totoloto')
+              return (
+                <div key={i}>
+                  <div className="new-line" style={{ marginBottom: 8 }}>
+                    {r.key}
+                  </div>
+                  <div className="new-line">{r.date}</div>
+                </div>
+              );
+            else return '';
+          })}
+          <br></br>
+          <div className="new-line" style={{ fontSize: 12 }}>
+            Update: {this.state.pubdate}
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default Popup;
